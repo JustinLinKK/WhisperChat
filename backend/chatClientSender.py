b84e38deff03f01ca5a482644ae5ddc4ac3d01c1
JustinLinKK <lin88@mcmaster.ca>
2023-03-04 23:55:56 -0500

    Change the client

--- backend/chatClientSender.py
+++ backend/chatClientSender.py
@@ -7,29 +7,18 @@
 #Enter the username with a ‘#’ prefix. Example: #alice
 #Send the message to a user by following the format @username:message. Example: @bob:Hello, Bob! This is alice
 #Repeat step 2 for other users. (Maximum 5 users is allowed with server configuration i.e. server_socket.listen(5)
+#Run reciever before use this script function
 
 import socket
 import encryption
+import chatClientReciever
+
 
 client_socket = socket.socket()
 port = 12345
 client_socket.connect(('127.0.0.1',port))
 
-# Receive connection message from server
-recv_msg = client_socket.recv(1024)
-print(recv_msg.decode('utf-8'))
 
-# Send user details to server
-username = input("Enter your user name (prefix with #): ").strip().encode('utf-8')
-client_socket.send(username)
-
-# Receive  message from different users
-def recv_message():
-    recv_msg = client_socket.recv(1024)
-    msg_body = recv_msg.split(b":", 1)[1]
-    private_key = encryption.read_private_key()
-    msg_decrypt = encryption.decrypt(msg_body)
-    print(recv_msg.decode('utf-8'))
 
 
 #Input message_body, userneame.
@@ -41,5 +30,8 @@ def send_message(send_msg,recipient):
     encrypt_message = encryption.encrypt(message,public_key)
     send_msg = "@"+recipient+":"+encrypt_message
     client_socket.send(send_msg.encode('utf-8'))
+
+
+
 
 
