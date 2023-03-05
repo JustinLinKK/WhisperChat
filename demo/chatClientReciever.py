6e78739e8c6db8be6f680d89794a526ba02455dd
JustinLinKK <lin88@mcmaster.ca>
2023-03-05 05:37:40 -0500

    Update chatClientReciever.py

--- demo/chatClientReciever.py
+++ demo/chatClientReciever.py
@@ -8,41 +8,18 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('192.168.2.15', 5555))
 server_socket.listen(5)
 
-
+c, address = server_socket.accept()
 
 
 
 
 while True:
-        c, address = server_socket.accept()
-        message = c.recv(2048).decode('utf-8')
-        if not message:
-            break
-        # Direct message from another user
-        if message.startswith("#"):
-            if encryption.read_public_key("localhost") == False:
-                private_key,public_key = encryption.generate_key_pairs
-                public_key_str= encryption.public_key_to_string(public_key)
-                encryption.store_private_key_file(private_key)
-                encryption.store_public_key_file(public_key,"localhost")
-            else:
-                public_key = encryption.read_public_key("localhost")
-                public_key_str= encryption.public_key_to_string(public_key)
-            send_msg = "@" + public_key_str
-            encryption.store_private_key_file(private_key)
-            server_socket.send(send_msg.encode('utf-8'))
-
-        elif message.startswith("@"):
-            public_key_str = message[1:]
-            public_key = encryption.public_string_to_key(public_key_str)
-            encryption.store_public_key_file(public_key,c.getsockname)
-
-        else:
-            private_key = encryption.read_private_key()
-            decrypt_message = encryption.decrypt(message,private_key)
-            recv_message = decrypt_message.decode('utf-8')
-            sender = c.getsockname()
-            print(sender, recv_message)
+        message = c.recv(2048)
+        private_key = encryption.read_private_key()
+        decrypt_message = encryption.decrypt(message,private_key)
+        recv_message = decrypt_message.decode('utf-8')
+        sender = c.getsockname()
+        print(sender, recv_message)
 
 server_socket.close()
         
